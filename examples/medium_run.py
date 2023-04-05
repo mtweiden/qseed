2ca352e0a310f726263c2d5a96fc8115e54f5154
Mathias Weiden <mtweiden@berkeley.edu>
2023-04-04 18:39:36 -0700

    Timing recommender

--- examples/medium_run.py
+++ examples/medium_run.py
@@ -8,19 +8,19 @@ import logging
 
 
 if __name__ == '__main__':
-    file_name = 'mapped-qft_64.qasm'
+    file_name = 'mapped-qft_16'
     logging.basicConfig(level=logging.INFO, filename=file_name)
 
     np.random.seed(1234)
 
-    qseed_circuit = Circuit.from_file(f'qasm/qft/{file_name}')
-    qsearch_circuit = Circuit.from_file(f'qasm/qft/{file_name}')
+    qseed_circuit = Circuit.from_file(f'qasm/qft/{file_name}.qasm')
+    #qsearch_circuit = Circuit.from_file(f'qasm/qft/{file_name}.qasm')
 
     qseed_handler = Handler()
-    qsearch_handler = QSearchHandler()
+    #qsearch_handler = QSearchHandler()
 
     qseed_data = {}
     qseed_handler.handle(qseed_circuit, qseed_data)
-    print('='*80)
-    qsearch_data = {}
-    qsearch_handler.handle(qsearch_circuit, qsearch_data)
+    #print('='*80)
+    #qsearch_data = {}
+    #qsearch_handler.handle(qsearch_circuit, qsearch_data)
