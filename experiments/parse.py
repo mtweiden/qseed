ff6306af4ef3d3f07d3bc5ce3b24ae12ebb3dd96
Mathias Weiden <mtweiden@berkeley.edu>
2023-04-10 14:39:09 -0700

    updated parse

--- experiments/parse.py
+++ experiments/parse.py
@@ -2,36 +2,44 @@ import re
 from os import listdir
 from argparse import ArgumentParser
 
-def find_stats(lines : list[str]) -> tuple[int, int, int, float]:
+def find_stats(lines : list[str], name : str) -> tuple[int, int, int, float]:
 	# Collect: 
 	#  cx count
 	#  u3 count
 	#  instantiation calls
 	#  compilation time
-	cx_pat = r'CNOTGate Count'
-	u3_pat = r'U3Gate Count'
+	cx_pat = r'Optimized cx gates'
+	u3_pat = r'Optimized u3 gates'
 	inst_pat = r'Instantiation Calls'
-	time_pat = r'Optimization time'
+	#time_pat = r'Optimization time'
+	time_pat = r'Synthesis run time'
 
 	int_str = r'\d+'
 	float_str = r'\d+\.\d+'
 
+	calls = []
 	cx, u3, inst, time = 0, 0, 0, 0.0
 
 	for line in lines:
 		if re.findall(cx_pat, line):
-			cx += int(re.findall(int_str, line)[0])
+			cx = int(re.findall(int_str, line)[1])
+			break
 
 		elif re.findall(u3_pat, line):
-			u3 += int(re.findall(int_str, line)[0])
+			u3 = int(re.findall(int_str, line)[1])
 
 		elif re.findall(inst_pat, line):
-			inst += int(re.findall(int_str, line)[0])
+			x = int(re.findall(int_str, line)[0])
+			if 'qseed' in name: 
+				x -= 1 # Don't count empty circuit instantiation
+			inst += x
+			calls += [x]
 
 		elif re.findall(time_pat, line):
 			time += float(re.findall(float_str, line)[0])
-			break # For circuits run multiple times
 
+	with open(f'calls/{name}', 'w') as f:
+		f.write(str(calls))
 	return cx, u3, inst, time
 
 def sort_files(files : list[str]) -> list[str]:
@@ -46,9 +54,6 @@ def sort_files(files : list[str]) -> list[str]:
 	return sorted_files
 
 if __name__ == '__main__':
-	#parser = ArgumentParser()
-	#parser.add_argument('log_file')
-	#args = parser.parse_args()
 
 	files = sort_files(listdir('logs/qseed'))
 
@@ -57,6 +62,6 @@ if __name__ == '__main__':
 		stats_file = 'stats.txt'
 		for algo in ['qseed', 'qsearch']:
 			with open(f'logs/{algo}/{name}.log', 'r') as f:
-				cx, u3, inst, time = find_stats(f.readlines())
+				cx, u3, inst, time = find_stats(f.readlines(), f'{algo}-{name}')
 			with open(stats_file, 'a') as f:
 				f.write(f'{name}, {algo}, {cx}, {u3}, {inst}, {time}\n')
